5bf990e1150aa81b3059ae71348c7b83ec3b0390
Andrii Artamonov <artamonov06@gmail.com>
2017-04-19 01:29:32 +0200

    first commit

--- app/src/main/java/com/example/artam/httpclient/MainActivity.java
+++ app/src/main/java/com/example/artam/httpclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.artam.httpclient;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -38,11 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myTag";
     Button button;
-    TextView text;
     EditText editText;
-    URL url;
-    String site;
     private WebView wv1;
+    private String concatUrl;
+    boolean connectionIsValid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,47 +57,101 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-
-        String url = editText.getText().toString().trim();
-        boolean checkUrl = isValidUrl(url);
-        if(checkUrl==true){
-            String testhttp = url.substring(0,4);
-            if(!testhttp.equals("http")){
-                String http="http://";
-                url=http.concat(url);
+                checkConnection(connectionIsValid);
+                if (connectionIsValid){
+                if (editText.getText().toString().isEmpty()){
+                Toast.makeText(getApplicationContext(), "Please enter your website!", Toast.LENGTH_SHORT).show();
             }
-            wv1.getSettings().setLoadsImagesAutomatically(true);
-            wv1.getSettings().setJavaScriptEnabled(true);
-            wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-            ////-------Exception Handling-------//////////
+            else {
+                String url = editText.getText().toString().trim();
+                boolean checkUrl = isValidUrl(url);
+                if (checkUrl == true) {                                        //source?
+                    final String testhttp = url.substring(0, 4);
+                    if (!testhttp.equals("http")) {
+                        String http = "http://";
+                        concatUrl = http.concat(url);
+                    }
+                    wv1.getSettings().setLoadsImagesAutomatically(true);
+                    wv1.getSettings().setJavaScriptEnabled(true);
+                    wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                    wv1.setWebViewClient(new WebViewClient() {
+                        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                            // Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), concatUrl + " doesn't exist", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    if (concatUrl.isEmpty()) {
+                        wv1.loadUrl(url);
+                    } else
+                        wv1.loadUrl(concatUrl);
 
-            wv1.setWebViewClient(new WebViewClient() {
-                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                    Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please give a valid URL",
+                            Toast.LENGTH_LONG).show();
+                    wv1.clearView();
                 }
-            });
-
-            wv1.loadUrl(url);
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Please give a valid URL",
-                    Toast.LENGTH_LONG).show();
-            wv1.clearView();
-        }
 
             }
+            }}
         });
     }
-    private class MyBrowser extends WebViewClient {
+    private class MyBrowser extends WebViewClient {  //http://stackoverflow.com/questions/7772409/set-loadurltimeoutvalue-on-webview
+        boolean timeout;
+        public MyBrowser(){
+            timeout = true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            timeout = false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+           new Thread(new Runnable() {
+               @Override
+               public void run() {
+                    try {
+                        Thread.sleep(2);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    if(timeout){
+                        Toast.makeText(getApplicationContext(), "Timeout error",
+                                Toast.LENGTH_SHORT).show();
+                    }
+               }
+           }).start();
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
     }
-    public boolean isValidUrl(String url) {
+    public boolean isValidUrl(String url) { //source???
         Pattern p = Patterns.WEB_URL;
         Matcher m = p.matcher(url.toLowerCase());
         return m.matches();
     }
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean checkConnection(boolean a){ //https://dzone.com/articles/checking-an-internet-connection-in-android-2
+        if(isOnline()){
+           return connectionIsValid = true;
+        }else{
+            Toast.makeText(getApplicationContext(), "Please check your Internet connection!", Toast.LENGTH_SHORT).show();
+            return connectionIsValid = false;
+
+        }
+    }
 }
+
